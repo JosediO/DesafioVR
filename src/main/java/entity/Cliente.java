71c9bcd0ae34bf1a37628a84574838367ffb92fb
Josedi O <josediuepb@gmail.com>
2025-03-10 22:08:19 -0300

    feat: Client entity getters and setters

--- src/main/java/entity/Cliente.java
+++ src/main/java/entity/Cliente.java
@@ -13,9 +13,7 @@ import lombok.Setter;
  *
  * @author Josedi
  */
-@Getter
-@Setter
-@AllArgsConstructor
+
 public class Cliente {
     
     private String id;
@@ -23,4 +21,42 @@ public class Cliente {
     private Double limite;
     private LocalDate fatura;
     
+    public Cliente(String id, String nome, Double limite, LocalDate fatura){
+        this.id = id;
+        this.nome = nome;
+       this.limite = limite;
+        this.fatura = fatura;
+   }
+    
+    public String getId(){
+        return id;
+    }
+    
+    public void setId(String id){
+        this.id = id;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public Double getLimite(){
+        return limite;
+    }
+    
+    public void setLimite(Double limite){
+        this.limite = limite;
+    }
+    
+    public LocalDate getFatura(){
+        return fatura;
+    }
+    
+    public void setFatura(LocalDate fatura){
+        this.fatura = fatura;
+    }
 }
