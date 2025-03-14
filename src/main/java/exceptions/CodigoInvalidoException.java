69e17ae6ea94da41c37061d6ae483ece6949183c
Josedi O <josediuepb@gmail.com>
2025-03-14 20:03:39 -0300

    feat: Exceptions

---
+++ src/main/java/exceptions/CodigoInvalidoException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author Josedi
+ */
+public class CodigoInvalidoException extends RuntimeException{
+    
+    public CodigoInvalidoException(String message) {
+        super(message);
+    }
+}
